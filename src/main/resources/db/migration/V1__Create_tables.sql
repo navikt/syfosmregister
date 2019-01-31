ca24ad6ff5a74978063fe5787e7fbae425c68ce3
Joakim kartveit <joakim.kartveit@nav.no>
2019-01-31 11:41:19 +0100

    Endrer på Sykmelding tabel, lar aktoerIDLege være null

--- src/main/resources/db/migration/V1__Create_tables.sql
+++ src/main/resources/db/migration/V1__Create_tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Sykmelding (
   id                     SERIAL PRIMARY KEY,
   aktoerIdPasient        VARCHAR(50) NOT NULL,
-  aktoerIdLege           VARCHAR(50) NULL,
+  aktoerIdLege           VARCHAR(50) NOT NULL,
   navLogId               VARCHAR(50) NOT NULL,
   msgId                  VARCHAR(50) NOT NULL,
   legekontorOrgNr        VARCHAR(50) NOT NULL,
---
+++ src/main/resources/db/migration/V2__Alter_aktoerIdLege_ column_sykmelding.sql
@@ -0,0 +1,1 @@
+ALTER TABLE Sykmelding ALTER COLUMN aktoerIdLege VARCHAR(50) NULL;
