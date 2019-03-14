8eab225449f5a2c4d7b2aece2956524da9b6b3cd
Joakim kartveit <joakim.kartveit@nav.no>
2019-03-14 12:50:22 +0100

    Bumping version to 1.0.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import com.github.jengelman.gradle.plugins.shadow.transformers.ServiceFileTransf
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "no.nav.syfo"
-version = "1.0.9"
+version = "1.0.10"
 
 val coroutinesVersion = "1.0.1"
 val javaxActivationVersion = "1.1.1"
