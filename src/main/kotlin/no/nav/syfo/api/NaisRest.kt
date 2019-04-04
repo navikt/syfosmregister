a6a3bf63ad300c7c70f223a01c74436a84151684
Martin Solheim <martin.solheim@me.com>
2019-04-04 09:25:17 +0200

    Configure endpoint for sykmeldinger

--- src/main/kotlin/no/nav/syfo/api/NaisRest.kt
+++ src/main/kotlin/no/nav/syfo/api/NaisRest.kt
@@ -9,21 +9,21 @@ import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.prometheus.client.CollectorRegistry
 import io.prometheus.client.exporter.common.TextFormat
+import no.nav.syfo.ApplicationState
 
 fun Routing.registerNaisApi(
-    readynessCheck: () -> Boolean,
-    livenessCheck: () -> Boolean = { true },
+    applicationState: ApplicationState,
     collectorRegistry: CollectorRegistry = CollectorRegistry.defaultRegistry
 ) {
     get("/is_alive") {
-        if (livenessCheck()) {
+        if (applicationState.running) {
             call.respondText("I'm alive! :)")
         } else {
             call.respondText("I'm dead x_x", status = HttpStatusCode.InternalServerError)
         }
     }
     get("/is_ready") {
-        if (readynessCheck()) {
+        if (applicationState.initialized) {
             call.respondText("I'm ready! :)")
         } else {
             call.respondText("Please wait! I'm not ready :(", status = HttpStatusCode.InternalServerError)
