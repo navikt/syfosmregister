72a0ecf9851dc5e8731c268daecc936b431418de
Joakim kartveit <joakim.kartveit@nav.no>
2019-05-09 09:40:11 +0200

    Prodsette versjon 1.0.26

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import com.github.jengelman.gradle.plugins.shadow.transformers.ServiceFileTransf
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "no.nav.syfo"
-version = "1.0.25"
+version = "1.0.26"
 
 val coroutinesVersion = "1.1.1"
 val javaxActivationVersion = "1.1.1"
