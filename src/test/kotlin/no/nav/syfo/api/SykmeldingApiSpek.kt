a8634aab1b579a6adfc996c4262b5bc1943de5c2
Joakim kartveit <joakim.kartveit@nav.no>
2019-05-22 13:59:10 +0200

    Legger til nytt felt i databsen tssid

--- src/test/kotlin/no/nav/syfo/api/SykmeldingApiSpek.kt
+++ src/test/kotlin/no/nav/syfo/api/SykmeldingApiSpek.kt
@@ -190,7 +190,8 @@ private fun Connection.setupTestData() {
                 tiltakNAV = "tiltakNAV",
                 utdypendeOpplysninger = emptyMap()
             ),
-            behandlingsUtfall = ValidationResult(Status.OK, emptyList())
+            behandlingsUtfall = ValidationResult(Status.OK, emptyList()),
+            tssid = "13455"
         )
     )
 }
