cfc7c7be5682d8f832c7a531d5cd291b59c92bfb
Tia Firing <tia.firing@nav.no>
2019-10-22 11:23:47 +0200

    Monitorerer latency for apiene

--- src/main/kotlin/no/nav/syfo/metrics/MetricRegister.kt
+++ src/main/kotlin/no/nav/syfo/metrics/MetricRegister.kt
@@ -1,6 +1,7 @@
 package no.nav.syfo.metrics
 
 import io.prometheus.client.Counter
+import io.prometheus.client.Histogram
 
 const val METRICS_NS = "syfosmregister"
 
@@ -9,3 +10,9 @@ val MESSAGE_STORED_IN_DB_COUNTER: Counter = Counter.build()
         .name("message_stored_in_db_count")
         .help("Counts the number of messages stored in db")
         .register()
+
+val HTTP_HISTOGRAM: Histogram = Histogram.Builder()
+    .labelNames("path")
+    .name("requests_duration_seconds")
+    .help("http requests durations for incoming requests in seconds")
+    .register()
