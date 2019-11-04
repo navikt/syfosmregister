0deda1468f02cbbd85c4a0220a284c90bdce85af
andreasnilsen <danduras@gmail.com>
2019-11-04 10:39:12 +0100

    event should not be null

--- src/main/resources/db/migration/V9__create_sykmelding_status_table.sql
+++ src/main/resources/db/migration/V9__create_sykmelding_status_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE sykmeldingstatus
 (
     sykmelding_id   VARCHAR(64) NOT NULL,
     event_timestamp TIMESTAMP   NOT NULL,
-    event           VARCHAR,
+    event           VARCHAR     NOT NULL,
 
     CONSTRAINT sykmeldingstatus_pk PRIMARY KEY (sykmelding_id, event_timestamp)
 );
