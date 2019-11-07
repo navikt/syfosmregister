d54813232febb5da6874ba2d520fbaa18e1074b8
andreasnilsen <danduras@gmail.com>
2019-11-07 14:40:11 +0100

    change to norwegian status

--- src/main/resources/db/migration/V10__migrate_data_to_sykmelding_status.sql
+++ src/main/resources/db/migration/V10__migrate_data_to_sykmelding_status.sql
@@ -1,7 +1,5 @@
 insert into sykmeldingstatus (sykmelding_id, event_timestamp, event)
-select id, mottatt_tidspunkt, 'OPEN' from sykmeldingsopplysninger on CONFLICT DO NOTHING;
+select id, mottatt_tidspunkt, 'APEN' from sykmeldingsopplysninger on CONFLICT DO NOTHING;
 
 insert into sykmeldingstatus (sykmelding_id, event_timestamp, event)
-select id, bekreftet_dato, 'CONFIRMED' from sykmeldingsmetadata where bekreftet_dato is not null on CONFLICT DO NOTHING;
-
-
+select id, bekreftet_dato, 'BEKREFTET' from sykmeldingsmetadata where bekreftet_dato is not null on CONFLICT DO NOTHING;
