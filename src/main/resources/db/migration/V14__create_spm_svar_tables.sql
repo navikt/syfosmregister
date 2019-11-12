c6d4743c94b3763b5bd88482171bc231e00107aa
Tia Firing <tia.firing@nav.no>
2019-11-12 14:27:26 +0100

    retter feil i primary/foreign key

--- src/main/resources/db/migration/V14__create_spm_svar_tables.sql
+++ src/main/resources/db/migration/V14__create_spm_svar_tables.sql
@@ -1,14 +1,16 @@
 CREATE TABLE sporsmal
 (
-    id      VARCHAR(64)   NOT NULL,
+    id      VARCHAR(64)   NOT NULL PRIMARY KEY,
     tekst   VARCHAR(255)  NOT NULL
 );
 
 CREATE TABLE svar
 (
-    id              VARCHAR(64)   NOT NULL,
+    id              VARCHAR(64)   NOT NULL PRIMARY KEY,
     sykmelding_id   VARCHAR(64)   NOT NULL,
-    sporsmal_id     VARCHAR(64)   NOT NULL REFERENCES sporsmal (id),
+    sporsmal_id     VARCHAR(64)   NOT NULL,
     svartype        VARCHAR(64)   NOT NULL,
-    verdi           VARCHAR(255)  NOT NULL
+    verdi           VARCHAR(255)  NOT NULL,
+
+    FOREIGN KEY (sporsmal_id) REFERENCES sporsmal (id)
 );
