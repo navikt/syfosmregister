ad0d6e7ec81312a29fd1a68aa254c0c9a00c026f
Tia Firing <tia.firing@nav.no>
2019-11-13 09:38:42 +0100

    setter primary key for arbeidsgiver

--- src/main/resources/db/migration/V13__create_arbeidsgiver_table.sql
+++ src/main/resources/db/migration/V13__create_arbeidsgiver_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE arbeidsgiver
 (
-    sykmelding_id        VARCHAR   NOT NULL,
+    sykmelding_id        VARCHAR   PRIMARY KEY REFERENCES sykmeldingsopplysninger (id),
     orgnummer            VARCHAR   NOT NULL,
     juridisk_orgnummer   VARCHAR,
     navn                 VARCHAR   NOT NULL
