7430d5eeadf5b8f6f5e032f7207e3ff28e26676f
Tia Firing <tia.firing@nav.no>
2019-11-13 08:45:33 +0100

    Forbedringer etter pr

--- src/main/resources/db/migration/V13__create_arbeidsgiver_table.sql
+++ src/main/resources/db/migration/V13__create_arbeidsgiver_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE arbeidsgiver
 (
-    sykmelding_id        VARCHAR(64)   NOT NULL,
-    orgnummer            VARCHAR(64)   NOT NULL,
-    juridisk_orgnummer   VARCHAR(64),
-    navn                 VARCHAR(255)  NOT NULL
+    sykmelding_id        VARCHAR   NOT NULL,
+    orgnummer            VARCHAR   NOT NULL,
+    juridisk_orgnummer   VARCHAR,
+    navn                 VARCHAR   NOT NULL
 );
--- src/main/resources/db/migration/V14__create_spm_svar_tables.sql
+++ src/main/resources/db/migration/V14__create_spm_svar_tables.sql
@@ -1,16 +1,17 @@
 CREATE TABLE sporsmal
 (
-    id      VARCHAR(64)   NOT NULL PRIMARY KEY,
-    tekst   VARCHAR(255)  NOT NULL
+    id          INT       GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    shortName   VARCHAR   NOT NULL,
+    tekst       VARCHAR   NOT NULL
 );
 
 CREATE TABLE svar
 (
-    id              VARCHAR(64)   NOT NULL PRIMARY KEY,
-    sykmelding_id   VARCHAR(64)   NOT NULL,
-    sporsmal_id     VARCHAR(64)   NOT NULL,
-    svartype        VARCHAR(64)   NOT NULL,
-    verdi           VARCHAR(255)  NOT NULL,
+    id              INT       GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    sykmelding_id   VARCHAR   NOT NULL,
+    sporsmal_id     INT       NOT NULL,
+    svartype        VARCHAR   NOT NULL,
+    svar            VARCHAR   NOT NULL,
 
     FOREIGN KEY (sporsmal_id) REFERENCES sporsmal (id)
 );
