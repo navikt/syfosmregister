a29e0e285f44edcd2f032c6397b6abe3ccd40a1f
Tia Firing <tia.firing@nav.no>
2019-12-12 12:45:29 +0100

    legger til indexer som vi vil trenge ved hent med status

---
+++ src/main/resources/db/migration/V17__add_sykmelding_id_index_on_statustabeller.sql
@@ -0,0 +1,5 @@
+create index sykmeldingstatus_sykmelding_id_idx on sykmeldingstatus (sykmelding_id);
+
+create index svar_sykmelding_id_idx on svar (sykmelding_id);
+
+create index arbeidsgiver_sykmelding_id_idx on arbeidsgiver (sykmelding_id);
