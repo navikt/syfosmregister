d692244e262de592aa0950da15689a99331780d9
andreasnilsen <danduras@gmail.com>
2020-01-07 14:15:12 +0100

    change id from varchar(64) to varchar, add index on sykmelding->>'msgId'

---
+++ src/main/resources/db/migration/V19__add_index_on_jsonb_and_increase_id_size.sql
@@ -0,0 +1,6 @@
+alter table sykmeldingsopplysninger alter column id type varchar;
+alter table sykmeldingsdokument alter column id type varchar;
+alter table behandlingsutfall alter column id type varchar;
+alter table sykmeldingstatus alter column sykmelding_id type varchar;
+
+create index on sykmeldingsdokument((sykmelding->>'msgId'));
