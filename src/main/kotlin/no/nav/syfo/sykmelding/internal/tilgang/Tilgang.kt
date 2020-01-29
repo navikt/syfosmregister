0aece28e506313f87d2144f9e33a5c65b3f2cbef
andreasnilsen <danduras@gmail.com>
2020-01-29 15:41:09 +0100

    begrunnelse kan være null

--- src/main/kotlin/no/nav/syfo/sykmelding/internal/tilgang/Tilgang.kt
+++ src/main/kotlin/no/nav/syfo/sykmelding/internal/tilgang/Tilgang.kt
@@ -1,3 +1,3 @@
 package no.nav.syfo.sykmelding.internal.tilgang
 
-data class Tilgang(val harTilgang: Boolean, val begrunnelse: String)
+data class Tilgang(val harTilgang: Boolean, val begrunnelse: String?)
