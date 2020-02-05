b4ccb6f16bfd0247de19a7da6daced6354a6a616
andreasnilsen <danduras@gmail.com>
2020-02-05 15:23:50 +0100

    Fix name on field

--- src/main/kotlin/no/nav/syfo/sykmelding/internal/model/SykmeldingStatusDTO.kt
+++ src/main/kotlin/no/nav/syfo/sykmelding/internal/model/SykmeldingStatusDTO.kt
@@ -6,5 +6,5 @@ data class SykmeldingStatusDTO(
     val statusEvent: String,
     val timestamp: ZonedDateTime,
     val arbeidsgiver: ArbeidsgiverStatusDTO?,
-    val sporsmal: List<SporsmalDTO>
+    val spormsalOgSvarListe: List<SporsmalDTO>
 )
