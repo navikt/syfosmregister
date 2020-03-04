64b7ba7bb42ba01f3419f1f70dfdcaf07037daa9
andreasnilsen <danduras@gmail.com>
2020-03-04 16:50:50 +0100

    Add kafka integration test

---
+++ src/test/kotlin/no/nav/syfo/testutil/TestApplication.kt
@@ -0,0 +1,33 @@
+package no.nav.syfo.testutil
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.StatusPages
+import io.ktor.http.HttpStatusCode
+import io.ktor.jackson.jackson
+import io.ktor.response.respond
+import io.ktor.server.testing.TestApplicationEngine
+import no.nav.syfo.log
+
+fun TestApplicationEngine.setUpTestApplication() {
+    start(true)
+    application.install(StatusPages) {
+        exception<Throwable> { cause ->
+            call.respond(HttpStatusCode.InternalServerError, cause.message ?: "Unknown error")
+            log.error("Caught exception", cause)
+        }
+    }
+    application.install(ContentNegotiation) {
+        jackson {
+            registerKotlinModule()
+            registerModule(JavaTimeModule())
+            configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+            configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        }
+    }
+}
