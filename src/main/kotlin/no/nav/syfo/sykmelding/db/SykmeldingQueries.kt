e24751ddaa7e40d88cc514654ce0a354f12c8b93
Tia Firing <tia.firing@nav.no>
2020-04-27 12:14:28 +0200

    Skal kunne snede sykmelding selv om behandlingsutfall mangler

--- src/main/kotlin/no/nav/syfo/sykmelding/db/SykmeldingQueries.kt
+++ src/main/kotlin/no/nav/syfo/sykmelding/db/SykmeldingQueries.kt
@@ -20,6 +20,11 @@ fun DatabaseInterface.getSykmeldingerMedId(id: String): SykmeldingDbModel? =
         return connection.getSykmeldingMedSisteStatusForId(id)
     }
 
+fun DatabaseInterface.getSykmeldingerMedIdUtenBehandlingsutfall(id: String): SykmeldingDbModelUtenBehandlingsutfall? =
+    connection.use { connection ->
+        return connection.getSykmeldingMedSisteStatusForIdUtenBehandlingsutfall(id)
+    }
+
 fun DatabaseInterface.hentSporsmalOgSvar(id: String): List<Sporsmal> {
     connection.use { connection ->
         return connection.hentSporsmalOgSvar(id)
@@ -88,6 +93,35 @@ private fun Connection.getSykmeldingMedSisteStatusForId(id: String): SykmeldingD
         it.executeQuery().toList { toSykmeldingDbModel() }.firstOrNull()
     }
 
+private fun Connection.getSykmeldingMedSisteStatusForIdUtenBehandlingsutfall(id: String): SykmeldingDbModelUtenBehandlingsutfall? =
+    this.prepareStatement(
+        """
+                    SELECT opplysninger.id,
+                    mottatt_tidspunkt,
+                    legekontor_org_nr,
+                    sykmelding,
+                    status.event,
+                    status.event_timestamp,
+                    arbeidsgiver.orgnummer,
+                    arbeidsgiver.juridisk_orgnummer,
+                    arbeidsgiver.navn
+                    FROM sykmeldingsopplysninger AS opplysninger
+                        INNER JOIN sykmeldingsdokument AS dokument ON opplysninger.id = dokument.id
+                        LEFT OUTER JOIN arbeidsgiver as arbeidsgiver on arbeidsgiver.sykmelding_id = opplysninger.id
+                        LEFT OUTER JOIN sykmeldingstatus AS status ON opplysninger.id = status.sykmelding_id AND
+                                                                   status.event_timestamp = (SELECT event_timestamp
+                                                                                             FROM sykmeldingstatus
+                                                                                             WHERE sykmelding_id = opplysninger.id
+                                                                                             ORDER BY event_timestamp DESC
+                                                                                             LIMIT 1)
+                    where opplysninger.id = ?
+                    and not exists(select 1 from sykmeldingstatus where sykmelding_id = opplysninger.id and event in ('SLETTET'));
+                    """
+    ).use {
+        it.setString(1, id)
+        it.executeQuery().toList { toSykmeldingDbModelUtenBehandlingsutfall() }.firstOrNull()
+    }
+
 fun ResultSet.toSykmeldingDbModel(): SykmeldingDbModel {
     return SykmeldingDbModel(sykmeldingsDokument = objectMapper.readValue(getString("sykmelding"), Sykmelding::class.java),
             id = getString("id"),
@@ -95,6 +129,15 @@ fun ResultSet.toSykmeldingDbModel(): SykmeldingDbModel {
             legekontorOrgNr = getString("legekontor_org_nr"),
             behandlingsutfall = objectMapper.readValue(getString("behandlingsutfall"), ValidationResult::class.java),
             status = getStatus()
+    )
+}
+
+fun ResultSet.toSykmeldingDbModelUtenBehandlingsutfall(): SykmeldingDbModelUtenBehandlingsutfall {
+    return SykmeldingDbModelUtenBehandlingsutfall(sykmeldingsDokument = objectMapper.readValue(getString("sykmelding"), Sykmelding::class.java),
+        id = getString("id"),
+        mottattTidspunkt = getTimestamp("mottatt_tidspunkt").toLocalDateTime(),
+        legekontorOrgNr = getString("legekontor_org_nr"),
+        status = getStatus()
     )
 }
 
