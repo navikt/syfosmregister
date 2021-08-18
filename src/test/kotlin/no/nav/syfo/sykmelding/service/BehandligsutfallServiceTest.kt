c5fdbaed04d0b99e3d96e19bc3bebe732f69640a
andreasnilsen <danduras@gmail.com>
2021-08-18 17:48:09 +0200

    Reduce kafka test times

--- src/test/kotlin/no/nav/syfo/sykmelding/service/BehandligsutfallServiceTest.kt
+++ src/test/kotlin/no/nav/syfo/sykmelding/service/BehandligsutfallServiceTest.kt
@@ -4,7 +4,6 @@ import io.mockk.every
 import io.mockk.mockkClass
 import io.mockk.spyk
 import java.time.Duration
-import java.util.Properties
 import kotlinx.coroutines.runBlocking
 import no.nav.syfo.Environment
 import no.nav.syfo.application.ApplicationState
@@ -14,49 +13,30 @@ import no.nav.syfo.model.Status
 import no.nav.syfo.model.ValidationResult
 import no.nav.syfo.persistering.erBehandlingsutfallLagret
 import no.nav.syfo.sykmelding.kafka.KafkaFactory
-import no.nav.syfo.sykmelding.kafka.util.JacksonKafkaDeserializer
 import no.nav.syfo.sykmelding.kafka.util.JacksonKafkaSerializer
-import no.nav.syfo.testutil.KAFKA_IMAGE_NAME
-import no.nav.syfo.testutil.KAFKA_IMAGE_VERSION
+import no.nav.syfo.testutil.KafkaTest
 import no.nav.syfo.testutil.TestDB
 import no.nav.syfo.testutil.dropData
 import org.amshove.kluent.shouldEqual
-import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.clients.producer.KafkaProducer
-import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.StringDeserializer
-import org.apache.kafka.common.serialization.StringSerializer
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
-import org.testcontainers.containers.KafkaContainer
-import org.testcontainers.containers.Network
-import org.testcontainers.utility.DockerImageName
 
 class BehandligsutfallServiceTest : Spek({
     val testDb = TestDB()
-    val kafka = KafkaContainer(DockerImageName.parse(KAFKA_IMAGE_NAME).withTag(KAFKA_IMAGE_VERSION)).withNetwork(Network.newNetwork())
-    kafka.start()
     val environment = mockkClass(Environment::class)
-    every { environment.applicationName } returns "application"
-    every { environment.sm2013InvalidHandlingTopic } returns "invalidTopic"
-    every { environment.sm2013ManualHandlingTopic } returns "manualTopic"
-    every { environment.kafkaSm2013AutomaticDigitalHandlingTopic } returns "automaticTopic"
-    every { environment.mottattSykmeldingKafkaTopic } returns "mottatttopic"
-    every { environment.sykmeldingStatusTopic } returns "statustopic"
-    every { environment.sm2013BehandlingsUtfallTopic } returns "behandlingsutfall"
+    every { environment.applicationName } returns "${BehandligsutfallServiceTest::class.simpleName}"
+    every { environment.sm2013InvalidHandlingTopic } returns "${environment.applicationName}-invalidTopic"
+    every { environment.sm2013ManualHandlingTopic } returns "${environment.applicationName}-manualTopic"
+    every { environment.kafkaSm2013AutomaticDigitalHandlingTopic } returns "${environment.applicationName}-automaticTopic"
+    every { environment.mottattSykmeldingKafkaTopic } returns "${environment.applicationName}-mottatttopic"
+    every { environment.sykmeldingStatusTopic } returns "${environment.applicationName}-statustopic"
+    every { environment.sm2013BehandlingsUtfallTopic } returns "${environment.applicationName}-behandlingsutfall"
     every { environment.cluster } returns "localhost"
-    val kafkaConfig = Properties()
-    kafkaConfig.let {
-        it["bootstrap.servers"] = kafka.bootstrapServers
-        it[ConsumerConfig.GROUP_ID_CONFIG] = "groupId"
-        it[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
-        it[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = JacksonKafkaDeserializer::class.java
-        it[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JacksonKafkaSerializer::class.java
-        it[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
-        it[ConsumerConfig.AUTO_OFFSET_RESET_CONFIG] = "earliest"
-    }
+    val kafkaConfig = KafkaTest.setupKafkaConfig()
     val applicationState = ApplicationState(true, true)
     val consumerProperties = kafkaConfig.toConsumerConfig(
             "${environment.applicationName}-consumer", valueDeserializer = StringDeserializer::class
@@ -87,7 +67,6 @@ class BehandligsutfallServiceTest : Spek({
 
     afterGroup {
         testDb.stop()
-        kafka.stop()
     }
 
     describe("Test BehandlingsuftallService") {
