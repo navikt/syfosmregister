f263ae97f5230f2361ac42daffd04b38a460a92a
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2022-08-08 13:46:47 +0200

    Logging api/v2/sykmeldinger endpoint for users

--- src/main/kotlin/no/nav/syfo/sykmelding/user/api/SykmeldingApiV2.kt
+++ src/main/kotlin/no/nav/syfo/sykmelding/user/api/SykmeldingApiV2.kt
@@ -10,10 +10,10 @@ import io.ktor.server.routing.accept
 import io.ktor.server.routing.get
 import io.ktor.server.routing.route
 import no.nav.syfo.application.BrukerPrincipal
+import no.nav.syfo.log
 import no.nav.syfo.sykmelding.service.SykmeldingerService
 import no.nav.syfo.sykmelding.status.StatusEventDTO
 import java.time.LocalDate
-import no.nav.syfo.log
 
 fun Route.registrerSykmeldingApiV2(sykmeldingerService: SykmeldingerService) {
     route("/sykmeldinger") {
