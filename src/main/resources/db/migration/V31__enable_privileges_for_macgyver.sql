29717dc5e8a0f228eed1a82c87bab4f1e1d80cd7
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2022-08-22 11:15:46 +0200

    Fixed copy pase error

--- src/main/resources/db/migration/V31__enable_privileges_for_macgyver.sql
+++ src/main/resources/db/migration/V31__enable_privileges_for_macgyver.sql
@@ -4,7 +4,7 @@ BEGIN
         IF EXISTS
             (SELECT 1 FROM pg_user where usename = 'macgyver')
         THEN
-            GRANT SELECT ON ALL TABLES IN SCHEMA PUBLIC TO "sparenajob";
+            GRANT SELECT ON ALL TABLES IN SCHEMA PUBLIC TO "macgyver";
 END IF;
 END
 $$;
