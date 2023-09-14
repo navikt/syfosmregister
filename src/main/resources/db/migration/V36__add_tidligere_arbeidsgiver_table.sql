9d5b69bd1ab716a42d4b5d972d731e3d896442de
Andreas Sagen Aspaas <danduras@gmail.com>
2023-09-14 11:13:21 +0200

    add tidligeer_arbeidsgiver table

--- src/main/resources/db/migration/V36__add_tidligere_arbeidsgiver_table.sql
+++ src/main/resources/db/migration/V36__add_tidligere_arbeidsgiver_table.sql
@@ -1,5 +1,5 @@
 create table tidligere_arbeidsgiver
 (
-    sykmeldingId           VARCHAR NOT null primary key,
+    sykmelding_id           VARCHAR NOT null primary key,
     tidligere_arbeidsgiver jsonb   not null
 )
